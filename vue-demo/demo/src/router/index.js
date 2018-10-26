f25fb4e0f5c616e938813b2176af491a89405b0a
陈伟潮 <996211950@qq.com>
2018-10-26 17:23:16 +0800

    路由

--- vue-demo/demo/src/router/index.js
+++ vue-demo/demo/src/router/index.js
@@ -1,15 +1,34 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+//一次性加载
+// import HelloWorld from '@/components/HelloWorld'
 
+//按需加载
+const HelloWorld = () => import('@/components/HelloWorld')
+const DynamicRouter = () => import('@/components/DynamicRouter')
+// const HelloWorld = (resolve) => { import( '../componets/HelloWorld').then((module) => { resolve(module)})}
+// const DynamicRouter = (resolve) => { import('../componets/DynamicRouter').then((module) => { resolve(module)} )}
 Vue.use(Router)
-
-export default new Router({
+const router  = new Router({
   routes: [
     {
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path:"/DynamicRouter/:id",
+      name:'DynamicRouter',
+      component: DynamicRouter
     }
   ]
 })
+
+router.beforeEach(function (to, from, next) {
+  if (to.path === '/DynamicRouter') {
+    next(false)
+  } else {
+    next()
+  }
+})
+export default router;
